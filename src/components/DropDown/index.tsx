57334a1426920f0d5472b7e637a1fab08a56073e
Colton-ProBuiltSoftware <coltonhillebrand92@icloud.com>
2025-07-11 13:23:11 -0500

    Revert "refactor(dropdown): add onLayout handler to DropDown component for measurement purposes"
    This reverts commit 16bae0bf1cd1bc59d7ad0aba6d004d0ca1546b11.

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -454,7 +454,7 @@ export const DropDown = React.forwardRef<IDropdownRef, DropdownProps<any>>(
           }}
           style={[styles.mainWrap, style]}
         >
-          <View ref={ref} style={styles.dropdown} onLayout={_measure}>
+          <View ref={ref} style={styles.dropdown}>
             {renderLeftIcon?.(visible)}
             <Text
               style={[
